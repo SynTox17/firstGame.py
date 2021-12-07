58c0fb874707a1b0dbcc64decca854faabeaf44b
andrew.p1 <pachnatzandrew@gmail.com>
2021-12-07 12:55:53 +0100

    lk2

--- Lf1/ISBN iwas.py
+++ Lf1/ISBN iwas.py
@@ -1,1 +1,4 @@
-
+while True:
+    y = input("Name")
+    if y.isdigit or y.isdecimal:
+        print(2+2)
---
+++ Lf1/While 02.py
@@ -0,0 +1,7 @@
+name = input("your name. SIR ")
+age = input("Your Age Sir")
+print(f"Hello {name} You are {age} old ")
+print("Hello" )
+tage = "Montag, Dienstag, Mittwoch , Donnerstag, Freitag, Samstag, Sonntag "
+for Wochentage in tage:
+    print(f"")
